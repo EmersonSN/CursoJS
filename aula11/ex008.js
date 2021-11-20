c29749c09e1d3814e138dd04e43377e6b3f6ee8d
EmersonSN <emersonn956@gmail.com>
2021-11-19 21:08:46 -0300

    Iniciando os estudos de condições Ainda seguindo os vídeos do Gustavo Guababara do curso em vídeo!

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let v = 50.5;
+console.log(`A sua velocidade atual é ${v}Km/h`);
+if(v>60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
